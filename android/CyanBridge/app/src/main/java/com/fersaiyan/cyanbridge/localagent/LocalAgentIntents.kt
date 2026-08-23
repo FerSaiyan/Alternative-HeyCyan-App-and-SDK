13dbaeeafb3ded43fc640844786c080dd30ef746
Akio Kenzo Tezuka Frazatto <96912395+FerSaiyan@users.noreply.github.com>
2026-08-23 13:58:36 -0300

    feat(localagent): add textual approval reply command

--- android/CyanBridge/app/src/main/java/com/fersaiyan/cyanbridge/localagent/LocalAgentIntents.kt
+++ android/CyanBridge/app/src/main/java/com/fersaiyan/cyanbridge/localagent/LocalAgentIntents.kt
@@ -12,6 +12,7 @@ object LocalAgentIntents {
     const val ACTION_DEMO = "com.fersaiyan.cyanbridge.localagent.action.DEMO"
     const val ACTION_READ_SCREEN_ALOUD = "com.fersaiyan.cyanbridge.localagent.action.READ_SCREEN_ALOUD"
     const val ACTION_GET_STATUS = "com.fersaiyan.cyanbridge.localagent.action.GET_STATUS"
+    const val ACTION_APPROVAL_REPLY = "com.fersaiyan.cyanbridge.localagent.action.APPROVAL_REPLY"
     const val ACTION_RESUME_AFTER_APPROVAL = "com.fersaiyan.cyanbridge.localagent.action.RESUME_AFTER_APPROVAL"
     const val ACTION_TELEGRAM_REMOTE_START = "com.fersaiyan.cyanbridge.localagent.action.TELEGRAM_REMOTE_START"
     const val ACTION_TELEGRAM_REMOTE_STOP = "com.fersaiyan.cyanbridge.localagent.action.TELEGRAM_REMOTE_STOP"
@@ -27,6 +28,7 @@ object LocalAgentIntents {
     const val EXTRA_TASK_ID = "task_id"
     const val EXTRA_IS_TERMINAL = "is_terminal"
     const val EXTRA_USER_MESSAGE = "user_message"
+    const val EXTRA_APPROVAL_REPLY = "approval_reply"
     const val EXTRA_DISABLE_REMOTE = "disable_remote"
     const val EXTRA_REJECTED = "rejected"
 }
